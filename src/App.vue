47036a61dcabe1721e9c7af35e0a26317ffa8e79
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-04-22 23:41:48 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <div style="position: fixed; top: 0; left: 0; width: 100%;  border-bottom: 1px solid #ccc;">
+    <h2>TODO Sharing</h2>
+  </div>
+
+  <div style="display: flex">
+    <div style="display: block;">
+      <div>TODOリストを新しく作成</div>
+      <div>
+        <input type="text" placeholder="合言葉を入力">
+        <button>作成</button>
+      </div>
+    </div>
+    <div style="display: block;">
+    <div>TODOリストを検索</div>
+    <div>
+        <input type="text" placeholder="合言葉を入力">
+        <button>検索</button>
+      </div>
+      </div>
+  </div>
+  <div class="container mt-5">
+    <h1 class="text-primary">Bootstrap 動作確認</h1>
+    <button class="btn btn-success">ボタン</button>
+  </div>
+
+
+  <!-- <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
