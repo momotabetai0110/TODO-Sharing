c5bad3540e74130de7c1024c82834d030b1c8328
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-04-26 21:35:25 +0900

    Todoページとルーティングの作成

---
+++ src/pages/TodoPage.vue
@@ -0,0 +1,42 @@
+<template>
+    <!-- Pages/TodoPage.vue -->
+     <div style="margin-bottom: 350px;">
+    <h2 style="color: aquamarine; margin-right:350px;">{{ TodoName }}</h2>
+
+    <div class="TodoList" style="background-color: bisque; display: block; font-size: larger;">
+        <div v-for="(item, index) in TodoItems" :key="index"
+            style="display: flex; justify-content: space-between; align-items: center;">
+            <div>{{ item }}</div>
+            <div style="align-items: end;">
+                <button class="btn btn-danger">削除</button>
+            </div>
+        </div>
+    </div>
+    <button class="btn btn-primary" @click="goBack">戻る</button>
+</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+const TodoName = ref('テストTODOname')
+const TodoItems = ref([
+    "タスク1",
+    "タスク2",
+    "タスク3",
+])
+
+onMounted(() => {
+    const keyword = route.query.keyword as string
+    if (keyword) {
+        TodoName.value = keyword
+    }
+})
+
+const goBack = () => {
+    router.push('/')
+}
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,18 @@
 // src/router/index.ts
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
+import TodoPage from '@/pages/TodoPage.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomePage
+  },
+  {
+    path: '/todo',
+    name: 'Todo',
+    component: TodoPage
   }
 ]
 
